afca65c75a2ce7336ee16f7d80155057ca63cb48
KiranKumarMalik <malikkiran413@gmail.com>
2023-05-17 23:45:16 +0530

    new project

---
+++ obcs/admin/includes/footer.php
@@ -0,0 +1,14 @@
+ <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                        <p> © Copyright&nbsp;<script>document.write(new Date().getFullYear())</script>. All Rights
+                        Reserved. Designed by <i class="fa fa-heart" style="font-size:25px;color:red"></i>&nbsp<b>Kiran Kumar Malik</b> </a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
---
+++ obcs/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <title>Home Page - Online Birth Certificate System</title>
+
+
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="css/responsive/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="loader">
+            <span class="inner1"></span>
+            <span class="inner2"></span>
+            <span class="inner3"></span>
+        </div>
+    </div>
+
+   
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area" id="header">
+        <div class="container-fluid h-100">
+            <div class="row h-100">
+                <div class="col-12 h-100">
+                    <nav class="h-100 navbar navbar-expand-lg align-items-center">
+                        <a class="navbar-brand" href="index.php"><img src="./img/cutm_logo.png" alt="Logo" width="60" height="75"></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#fancyNav" aria-controls="fancyNav" aria-expanded="false" aria-label="Toggle navigation"><span class="ti-menu"></span></button>
+                        <div class="collapse navbar-collapse" id="fancyNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                             
+                                <li class="nav-item">
+                                    <a class="nav-link" href="user/login.php">User</a>
+                                </li>
+                                                                <li class="nav-item">
+                                    <a class="nav-link" href="user/search.php">Verify Certifcate</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="admin/login.php">Admin</a>
+                                </li>
+                             
+                            </ul>
+                         
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Hero Area Start ***** -->
+    <div class="fancy-hero-area bg-img bg-overlay animated-img" style="background-image: url(img/poster.jpg);">
+       
+    </div>
+    <!-- ***** Hero Area End ***** -->
+
+  
+
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="fancy-footer-area fancy-bg-dark">
+      
+        <!-- Footer Copywrite -->
+        <div class="footer-copywrite-area">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="copywrite-content h-100 d-flex align-items-center justify-content-between">
+                            <!-- Copywrite Text -->
+                            <div class="copywrite-text">
+                            <p> © Copyright&nbsp;<script>document.write(new Date().getFullYear())</script>. All Rights
+                        Reserved. Designed by <i class="fa fa-heart" style="font-size:25px;color:red"></i>&nbsp<b>Kiran Kumar Malik</b> </a></p>
+                            </div>
+                            <!-- Footer Nav -->
+                                    
+                                        <li style="color: white;">Centurion University of Technology and Management</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap-4 js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/others/plugins.js"></script>
+    <!-- Active JS -->
+    <script src="js/active.js"></script>
+</body>
